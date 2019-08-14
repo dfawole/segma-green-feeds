a40508d751b1f51fe3e54f808d2f61e55ca7e11a
OlaFawole <Oladotun.Fawole@eurostar.com>
2019-08-15 00:39:59 +0100

    broken homepage into smaller components, finished styling menu-item component

---
+++ src/pages/homepage/homepage.component.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import Directory from '../../components/directory/direcroty.component'
+
+import './homepage.styles.scss';
+
+
+
+const HomePage = () => (
+    <div className='homepage'>
+        <Directory />
+    </div>
+
+);
+
+export default HomePage;
